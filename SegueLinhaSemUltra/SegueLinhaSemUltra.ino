7d5ffba47652472cf680a309a99c6048e46db287
Davi Santiago Moura <145844746+davisantiago10@users.noreply.github.com>
2025-08-07 11:59:59 -0300

    Update SegueLinhaSemUltra.ino

--- SegueLinhaSemUltra/SegueLinhaSemUltra.ino
+++ SegueLinhaSemUltra/SegueLinhaSemUltra.ino
@@ -5,13 +5,13 @@ const int sensorPinD = A0; // Sensor da direita
 const int sensorPinE = A1; // Sensor da esquerda
 
 // Ponte H L298N (Motores)
-#define ENA 5     // PWM motor esquerdo
-#define IN1 6
-#define IN2 10
+#define ENA 2     // PWM motor esquerdo
+#define IN1 3
+#define IN2 4
 
-#define ENB 11    // PWM motor direito
-#define IN3 12
-#define IN4 13
+#define ENB 7    // PWM motor direito
+#define IN3 5
+#define IN4 6
 
 void setup() {
   Serial.begin(9600);
@@ -29,7 +29,6 @@ void setup() {
   pinMode(IN4, OUTPUT);
 
   Serial.println("Sistema Iniciado: Sensor IR + Ultrassônico + Motores");
-  Serial.println("=======================KKKBURROKKK==============================");
 }
 
 void loop() {
@@ -44,29 +43,33 @@ void loop() {
   Serial.println(sensorValorDireita);
   Serial.print("Valor do sensor IR Esquerda: ");
   Serial.println(sensorValorEsquerda);
-
-  if (sensorValorEsquerda > 800 && sensorValorDireita > 800) {
+.
+  if (sensorValorEsquerda > 600 && sensorValorDireita > 600) {
     Serial.println(">> Superfície PRETA detectada");
     motorFrente();
   } 
 
-  else if (sensorValorEsquerda < 100 && sensorValorDireita < 100){
+  else if (sensorValorEsquerda < 200 && sensorValorDireita < 200){
     Serial.println(">> Superfície BRANCA detectada");
     motorFrente();
   }
 
-  else if (sensorValorEsquerda < 100 && sensorValorDireita > 800) {
+  else if (sensorValorEsquerda < 200 && sensorValorDireita > 600) {
     Serial.println(">> Virando para a direita");
     motorDireita();
   }
 
-  else if (sensorValorEsquerda > 800 && sensorValorDireita < 100){
+  else if (sensorValorEsquerda > 600 && sensorValorDireita < 200){
     Serial.println("Virando para a esquerda");
     motorEsquerda();
   }
+  else{
+    Serial.println("Cor não identificada");
+    motorFrente();
+  }
   // ------------------------------
   // LÓGICA DE MOVIMENTO DOS MOTORES
-  delay(500); // Aguarda antes da próxima leitura
+  delay(40); // Aguarda antes da próxima leitura
 }
 
 // ------------------------------
@@ -74,23 +77,13 @@ void loop() {
 // ------------------------------
 
 void motorFrente() {
-  digitalWrite(IN1, HIGH);
+  digitalWrite(IN1, HIGH);  // motor esquerdo para frente
   digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
+  digitalWrite(IN3, HIGH);  // motor direito para frente
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, 200); // Velocidade ajustável
-  analogWrite(ENB, 200);
+  analogWrite(ENA, 90);
+  analogWrite(ENB, 90);
   Serial.println(">> Motores girando para FRENTE");
-}
-
-void motorParar() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
-  Serial.println(">> Motores PARADOS");
 }
 
 void motorEsquerda(){
@@ -98,8 +91,8 @@ void motorEsquerda(){
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, 150); // Reduz um pouco a velocidade para curva
-  analogWrite(ENB, 200);
+  analogWrite(ENA, 40); // Inverte o movimento
+  analogWrite(ENB, 150);
   Serial.println(">> Motores girando para a ESQUERDA");
 }
 
@@ -107,9 +100,10 @@ void motorEsquerda(){
 void motorDireita(){
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
+  digitalWrite(IN3, LOW);   
   digitalWrite(IN4, HIGH);
-  analogWrite(ENA, 200);
-  analogWrite(ENB, 150);
+  analogWrite(ENA, 150);
+  analogWrite(ENB, 40);
   Serial.println(">> Motores girando para a DIREITA");
 }
+
